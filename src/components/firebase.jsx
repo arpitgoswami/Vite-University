aa642b9b880c174f0900c3c87ba8e0de97ee1f44
arpitgoswami <arpitgoswami@hotmail.com>
2023-10-13 22:40:37 +0530

    Commit

---
+++ src/components/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDryALE1MDFl2B2mQr6DHV1Fen-Gp8iC3M",
+  authDomain: "fsddsfsd.firebaseapp.com",
+  projectId: "fsddsfsd",
+  storageBucket: "fsddsfsd.appspot.com",
+  messagingSenderId: "52663122161",
+  appId: "1:52663122161:web:6777ee7aee3ac4d8be5150",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,72 +1,20 @@
-import React, { useState } from "react";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
 
-function Login() {
-  return (
-    <>
-      <div className="min-h-screen flex items-center justify-center bg-gray-800">
-        <div className="bg-white p-8 rounded-lg shadow-lg w-96">
-          <h2 className="text-3xl font-semibold mb-6 text-center text-gray-800">
-            Welcome Back!
-          </h2>
-          <form className="space-y-4">
-            <div>
-              <label className="block text-sm font-medium text-gray-600">
-                I am a:
-              </label>
-              <div className="flex items-center space-x-4">
-                <label className="flex items-center text-sm cursor-pointer">
-                  <input
-                    type="radio"
-                    name="userType"
-                    value="teacher"
-                    className="form-radio text-blue-500 focus:ring focus:border-blue-300"
-                  />
-                  <span className="ml-2">Teacher</span>
-                </label>
-                <label className="flex items-center text-sm cursor-pointer">
-                  <input
-                    type="radio"
-                    name="userType"
-                    value="student"
-                    className="form-radio text-blue-500 focus:ring focus:border-blue-300"
-                  />
-                  <span className="ml-2">Student</span>
-                </label>
-              </div>
-            </div>
-            <div>
-              <label className="block text-sm font-medium text-gray-600">
-                Username
-              </label>
-              <input
-                type="text"
-                name="username"
-                className="mt-1 p-3 w-full border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-sm font-medium text-gray-600">
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                className="mt-1 p-3 w-full border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-                required
-              />
-            </div>
-            <button
-              type="submit"
-              className="w-full bg-blue-500 hover:bg-blue-600 text-white p-3 rounded-md transition duration-300 ease-in-out focus:outline-none focus:ring focus:border-blue-300"
-            >
-              Login
-            </button>
-          </form>
-        </div>
-      </div>
-    </>
-  );
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  //...
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Get a list of cities from your database
+async function getCities(db) {
+  const citiesCol = collection(db, "cities");
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map((doc) => doc.data());
+  return cityList;
 }
-
-export default Login;
