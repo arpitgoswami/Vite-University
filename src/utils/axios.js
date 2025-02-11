63f2999b085dd4e0a48eb89d9c0549cceafdb50e
arpitgoswami <arpitgoswami@hotmail.com>
2025-02-11 11:58:23 +0530

    Initial Message

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 // http://localhost:3000/
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'https://aplex-backend-production.up.railway.app/',
 
     headers: {
         'Content-Type': 'application/json',
